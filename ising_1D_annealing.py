bf3bb2b5934be876e97a0f4e6923dcf65eca8b1e
Amin Ahmadi <ahmadi.amin.a@gmail.com>
2020-11-24 10:41:28 +0100

    chain flipped fixed

--- ising_1D_annealing.py
+++ ising_1D_annealing.py
@@ -28,24 +28,32 @@ def flip_random_n_v2(chain,n):
     return (chain + index)%2
 
 ##########
-def energy(chain, interaction = 1):
-    """ Evaluate the free energy of a chain based on given interaction"""
+def external_interaction(chain, ext_field = 1.):
+    """ Evaluate the energy of a chain in a presence of applied magnetic field"""
     # simple intereaction with external field
     # we know: ground state when all are aligned with the field.
     aux = interaction * 2*(chain-0.5).sum()
     return aux
 
+##########
+def z_interaction(chain, z_coeff = 1.):
+    """ Evaluare the energy of chain in presence of nearest-neightbor 
+    manetic moment interaction
+    """
+    spin_chain = (2*(0.5 - chain)).astype(int)
+    return z_coeff*(spin_chain[:-1] * spin_chain[1:]).sum()
 
+########## 
 def anneal(chain, iterations, cooling_rate):
     temperature = float(chain.size)
     while temperature > 0.5:
-        min_energy = energy(chain)
+        min_energy = z_interaction(chain)
         for i in range(iterations):
-            new_chain = flip_random_n_v2(chain, round(temperature))
-            new_energy = energy(new_chain)
+            disturbed_chain = flip_random_n(chain, round(temperature))
+            new_energy = z_interaction(disturbed_chain)
             if new_energy < min_energy:
-                min_energy, chain = new_energy, new_chain
-        # min_energy, chain = new_energy, new_chain
+                min_energy, new_chain = new_energy, disturbed_chain
+        new_energy, chain = min_energy, new_chain
         temperature = temperature / cooling_rate
         print(temperature, chain, min_energy)
     return chain
